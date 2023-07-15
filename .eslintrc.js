61a4a7c6142f570e6cb58907374ed5d970026358
Юлия Гвоздева <yulya96gvozdeva96@gmail.com>
2023-07-15 14:38:05 +0400

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+		'project': './tsconfig.json',
+	},
+	'plugins': [
+		'@typescript-eslint',
+		'react-hooks',
+		'react',
+		'prettier'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			2
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-empty-function': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'react/display-name': 'off',
+		'react/prop-types': 'off',
+		'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+		'arrow-body-style': ['error', 'as-needed'],
+		'react/jsx-boolean-value': ['error', 'always'],
+		'react/jsx-curly-brace-presence': [
+			'error',
+			{ props: 'always', children: 'always' },
+		],
+		'prettier/prettier': 'error',
+	}
+};
